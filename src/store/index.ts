cea011082ecd68135ab1e27a63023aa645280366
ishidafuu <ishidafuu@gmail.com>
2023-07-12 06:02:42 +0900

    Undoリファクタ

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,8 +4,8 @@ interface State {
     charIndex: number;
     offsetX: number;
     offsetY: number;
-    past: Array<{charIndex: number, offsetX: number, offsetY: number}>;
-    future: Array<{charIndex: number, offsetX: number, offsetY: number}>;
+    past: Array<any>;
+    future: Array<any>;
 }
 
 export const store = createStore<State>({
@@ -18,45 +18,39 @@ export const store = createStore<State>({
     },
     mutations: {
         increment(state) {
-            state.past = [...state.past, { charIndex: state.charIndex, offsetX: state.offsetX, offsetY: state.offsetY }];
+            state.past.push({ ...state });
             state.future = [];
             state.charIndex++;
         },
         decrement(state) {
             if (state.charIndex > 0) {
-                state.past = [...state.past, { charIndex: state.charIndex, offsetX: state.offsetX, offsetY: state.offsetY }];
+                state.past.push({ ...state });
                 state.future = [];
                 state.charIndex--;
             }
         },
         setOffsetX(state, value) {
-            state.past = [...state.past, { charIndex: state.charIndex, offsetX: state.offsetX, offsetY: state.offsetY }];
+            state.past.push({ ...state });
             state.future = [];
             state.offsetX = value;
         },
         setOffsetY(state, value) {
-            state.past = [...state.past, { charIndex: state.charIndex, offsetX: state.offsetX, offsetY: state.offsetY }];
+            state.past.push({ ...state });
             state.future = [];
             state.offsetY = value;
         },
         undo(state) {
             if (state.past.length > 0) {
-                const previousState = state.past[state.past.length - 1];
-                state.past = state.past.slice(0, state.past.length - 1);
-                state.future = [{ charIndex: state.charIndex, offsetX: state.offsetX, offsetY: state.offsetY }, ...state.future];
-                state.charIndex = previousState.charIndex;
-                state.offsetX = previousState.offsetX;
-                state.offsetY = previousState.offsetY;
+                const previousState = state.past.pop();
+                state.future.push({ ...state });
+                Object.assign(state, previousState);
             }
         },
         redo(state) {
             if (state.future.length > 0) {
-                const nextState = state.future[0];
-                state.future = state.future.slice(1);
-                state.past = [...state.past, { charIndex: state.charIndex, offsetX: state.offsetX, offsetY: state.offsetY }];
-                state.charIndex = nextState.charIndex;
-                state.offsetX = nextState.offsetX;
-                state.offsetY = nextState.offsetY;
+                const nextState = state.future.pop();
+                state.past.push({ ...state });
+                Object.assign(state, nextState);
             }
         },
     }
