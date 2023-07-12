6aa6ab4f300dcc8f9c2648f8a2fdd137ea6e7bbd
ishidafuu <ishidafuu@gmail.com>
2023-07-12 10:09:40 +0900

    顔View透過処理

--- src/components/CharacterView/FaceView.vue
+++ src/components/CharacterView/FaceView.vue
@@ -9,6 +9,7 @@ export default {
   data() {
     return {
       imagePath: '/face.png',
+      transparentImagePath: null,
       gridSize: 16
     }
   },
@@ -20,7 +21,7 @@ export default {
       const bgPosX = -col * this.gridSize;
       const bgPosY = -row * this.gridSize;
       return {
-        backgroundImage: `url(${this.imagePath})`,
+        backgroundImage: `url(${this.transparentImagePath || this.imagePath})`,
         backgroundPosition: `${bgPosX}px ${bgPosY}px`
       };
     },
@@ -34,7 +35,42 @@ export default {
         transform: `translate(${translateX}px, ${translateY}px) scale(${scale})`
       }
     }
-  }
+  },
+  mounted() {
+    this.makeColorTransparent(this.imagePath, [255, 0, 255], (transparentImg) => {
+      this.transparentImagePath = transparentImg;
+    });
+  },
+  methods: {
+    makeColorTransparent(image, color, callback) {
+      const canvas = document.createElement('canvas');
+      const context = canvas.getContext('2d');
+
+      const img = new Image();
+      img.onload = () => {
+        canvas.width = img.width;
+        canvas.height = img.height;
+
+        context.drawImage(img, 0, 0, img.width, img.height);
+
+        const imageData = context.getImageData(0, 0, img.width, img.height);
+        const data = imageData.data;
+
+        for (let i = 0; i < data.length; i += 4) {
+          if (data[i] === color[0] && data[i + 1] === color[1] && data[i + 2] === color[2]) {
+            // Make the pixel transparent
+            data[i + 3] = 0;
+          }
+        }
+
+        context.putImageData(imageData, 0, 0);
+
+        callback(canvas.toDataURL());
+      };
+
+      img.src = image;
+    }
+  },
 }
 </script>
 
