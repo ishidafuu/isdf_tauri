e851dbb626110ae890dc0ca832bb060d7f5d348b
ishidafuu <ishidafuu@gmail.com>
2023-07-23 17:37:39 +0900

    自動データ読み込み

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,26 @@
   </div>
 </template>
 
-<script>
+<script lang="ts">
+import { defineComponent, onMounted } from 'vue'
+import { useStore } from 'vuex'
 import CharacterView from './components/CharacterView.vue'
 import ControlView from "./components/ControlView.vue";
 
-export default {
+export default defineComponent({
   components: {
     CharacterView,
     ControlView,
   },
-}
-</script>
+  setup() {
+    const store = useStore()
 
+    onMounted(async () => {
+      await store.dispatch('loadState')
+    })
+  }
+})
+</script>
 
 <style scoped>
 .parent-component {
