487951185e0f1fd33099a97c9fa1c073f73b5c11
ishidafuu <ishidafuu@gmail.com>
2023-07-24 22:41:56 +0900

    バックアップ処理修正

--- src/store/index.ts
+++ src/store/index.ts
@@ -50,17 +50,12 @@ const initialBodyState: BodyState = {
 
 function getFormattedDate() {
     const date = new Date();
-    const year = date.getFullYear();
     const month = ("0" + (date.getMonth() + 1)).slice(-2);
     const day = ("0" + date.getDate()).slice(-2);
     const hours = ("0" + date.getHours()).slice(-2);
     const minutes = ("0" + date.getMinutes()).slice(-2);
     const seconds = ("0" + date.getSeconds()).slice(-2);
-    return `${year}${month}${day}${hours}${minutes}${seconds}`;
-}
-
-function getFilePath(dirPath: string, fileName: string) {
-    return `${dirPath}${fileName}`;
+    return `${month}${day}_${hours}${minutes}${seconds}`;
 }
 
 
