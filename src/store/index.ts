e4c9d6072b14494e7cea3dff61e1422d460d3c0f
ishidafuu <ishidafuu@gmail.com>
2023-09-18 20:33:00 +0900

    delBasemotion

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,7 @@
 import {createStore} from 'vuex';
 import charaCell from './charaCellEditor.ts';
 import errorHandler from './errorHandler.ts';
-import { createMotionStore } from './createMotionStore';
+import {createMotionStore} from './createMotionStore';
 import {Motion} from '../types/interface.ts';
 
 const baseMotion = createMotionStore<Motion>('base_motion_data', false);
